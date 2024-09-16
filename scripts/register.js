64767b3e73b2f44ee3f4ad0e6ee5c17731d07bc8
odeifuo <quasiahama@gmail.com>
2024-09-16 11:44:23 +0000

    first commit

---
+++ scripts/register.js
@@ -0,0 +1,10 @@
+// select form by id 
+const registerForm = document.getElementById(register-form); 
+// handle a form submit event
+registerForm.onsubmit =function() {
+    // collect username
+    // collect email 
+    // collect password 
+    // save user information
+    // send confirmation email
+}
